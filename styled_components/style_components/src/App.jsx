f298237ee8ef4393fffb8a22b7c364cc293170ce
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-04-25 02:07:25 +0530

    assignment added

--- styled_components/style_components/src/App.jsx
+++ styled_components/style_components/src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Div } from "./components/Div";
+// import { Button } from "./components/Div";
 import { Button } from "antd";
 function App() {
   return (
     <div className="App">
-      <Div>
+      <Div className="one">
         <Button type="primary">Primary Button</Button>
         <br />
         <Button>Default Button</Button> <br />
         <Button type="dashed">Dashed Button</Button> <br />
       </Div>
-      <Div>
+      <Div className="two">
         <Button type="text">Text Button</Button>
         <br />
         <Button type="link">Text Button</Button>
--- styled_components/style_components/src/components/Div.jsx
+++ styled_components/style_components/src/components/Div.jsx
@@ -1,4 +1,11 @@
 import styled from "styled-components";
-const Div = styled.div``;
+
+const Div = styled.div`
+  display: flex;
+  /* justify-content: center; */
+  justify-content: center;
+  text-align: center;
+  margin-top: 20px;
+`;
 
 export { Div };
