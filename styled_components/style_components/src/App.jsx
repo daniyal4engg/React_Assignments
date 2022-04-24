665f8edec6fc9e505428f026fedf7ae66ef70e7c
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-04-24 23:46:50 +0530

    assignment added

---
+++ styled_components/style_components/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Div } from "./components/Div";
+import { Button } from "antd";
+function App() {
+  return (
+    <div className="App">
+      <Div>
+        <Button type="primary">Primary Button</Button>
+        <br />
+        <Button>Default Button</Button> <br />
+        <Button type="dashed">Dashed Button</Button> <br />
+      </Div>
+      <Div>
+        <Button type="text">Text Button</Button>
+        <br />
+        <Button type="link">Text Button</Button>
+      </Div>
+    </div>
+  );
+}
+
+export default App;
