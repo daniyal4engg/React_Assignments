c0a4409d7ba8428806cf62359837381683a02b89
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-05-23 00:00:45 +0530

    assignment added

---
+++ RCT-121/rec121_3-1-github/gitHub/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Github } from "./components/github";
+import { Timer } from "./components/Timer";
+export default function App() {
+  return (
+    <div className="App">
+      <Github />
+      {/* <Timer /> */}
+    </div>
+  );
+}
