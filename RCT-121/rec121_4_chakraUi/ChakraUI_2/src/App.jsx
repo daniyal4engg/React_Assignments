955386df06ab0ed7eadef7aacb103b6f5c9d368f
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-05-25 23:54:02 +0530

    Chakra assignment 2 added

---
+++ RCT-121/rec121_4_chakraUi/ChakraUI_2/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Box,
+  Container,
+  Center,
+  SimpleGrid,
+  Grid,
+  GridItem,
+  Flex,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
+function App() {
+  return (
+    <div className="App">
+      <Box>
+        <Text>Desktop Layout</Text>
+      </Box>
+      <Center>
+        <Box>
+          <Grid
+            mt="50px"
+            w={["200px", "500px"]}
+            h="200px"
+            templateRows="repeat(3, 1fr)"
+            templateColumns="repeat(3, 1fr)"
+            gap={4}
+          >
+            <GridItem rowSpan={[1, 2]} colSpan={[3, 2]} bg="mediumpurple" />
+            <GridItem rowSpan={[1, 3]} colSpan={[3, 1]} bg="tomato" />
+            <GridItem rowSpan={[1, 1]} colSpan={[3, 2]} bg="lightskyblue" />
+          </Grid>
+        </Box>
+      </Center>
+    </div>
+  );
+}
+
+export default App;
--- RCT-121/rec121_4_chakraUi/chakraUi/src/App.jsx
+++ RCT-121/rec121_4_chakraUi/chakraUi/src/App.jsx
@@ -1,45 +1,102 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  Box,
+  Container,
+  Center,
+  SimpleGrid,
+  Grid,
+  GridItem,
+  Flex,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [orangToRed, setOrangeToRed] = "orange";
+  const [redToOrange, setRedToOrange] = "red";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <Text>Desktop Layout</Text>
+      <Center>
+        <Grid
+          w={["400px", "800px"]}
+          templateColumns="repeat(4, 1fr)"
+          templateRows="repeat(4, 1fr)"
+          gap={1}
+        >
+          <GridItem
+            rowSpan={[1, 4]}
+            colSpan={[4, 1]}
+            h="10"
+            bg="blue"
+            p="10px"
+            textAlign={"center"}
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            Nav
+          </GridItem>
+          <GridItem
+            rowSpan={[1, 4]}
+            colSpan={[4, 2]}
+            h="10"
+            bg="orange"
+            p="10px"
+            textAlign={"center"}
           >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+            Content
+          </GridItem>
+          <GridItem
+            rowSpan={[1, 4]}
+            colSpan={[4, 1]}
+            h="10"
+            bg="red"
+            p="10px"
+            textAlign={"center"}
+          >
+            Widget
+          </GridItem>
+        </Grid>
+      </Center>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// <Grid
+// h="200px"
+// templateRows="repeat(2, 1fr)"
+// templateColumns="repeat(3, 1fr)"
+// gap={1}
+// >
+// <GridItem rowSpan={2} colSpan={1} bg={["tomato", "steelblue"]}>
+//   Nav
+// </GridItem>
+// <GridItem rowSpan={2} colSpan={1} bg={["orange"]}>
+//   content
+// </GridItem>
+// <GridItem rowSpan={2} colSpan={1} bg={["red"]}>
+//   widget
+// </GridItem>
+// </Grid>
+
+// <Stack direction={["row", "Column"]}>
+// // <Box bg={["steelblue"]}>1</Box>
+// // <Box bg={["orange"]}>2</Box>
+// // <Box bg={["red"]}>3</Box>
+// </Stack>
+
+// <Box>
+// <Grid
+//   mt="50px"
+//   w={["200px", "500px"]}
+//   h="200px"
+//   templateRows="repeat(3, 1fr)"
+//   templateColumns="repeat(3, 1fr)"
+//   gap={4}
+// >
+//   <GridItem rowSpan={[1, 2]} colSpan={[3, 2]} bg="mediumpurple" />
+//   <GridItem rowSpan={[1, 3]} colSpan={[3, 1]} bg="tomato" />
+//   <GridItem rowSpan={[1, 1]} colSpan={[3, 2]} bg="lightskyblue" />
+// </Grid>
+// </Box>
