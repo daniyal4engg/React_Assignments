fc742d8cb69254b1fbb03694248d3937987ff04d
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-05-29 11:23:29 +0530

    Chakra assignment 2 added

--- RCT-121/rec121_4_chakraUi/chakra-Dashboard/src/components/Navbar/Navbar.jsx
+++ RCT-121/rec121_4_chakraUi/chakra-Dashboard/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 // import { SearchIcon } from "./SearchIcon";
 import { PhoneIcon, AddIcon, WarningIcon } from "@chakra-ui/icons";
 // import { IconName } from "react-icons/ai";
+import { AiOutlineFire, AiFillPushpin } from "react-icons/ai";
+import { BiWorld } from "react-icons/bi";
+import { FcBookmark } from "react-icons/fc";
 import {
   Box,
   Stack,
@@ -21,6 +24,7 @@ import {
   SimpleGrid,
   IconButton,
   Select,
+  Divider,
   // SearchIcon,
 } from "@chakra-ui/react";
 // import "./Navbar.css";
@@ -111,19 +115,279 @@ export const Navbar = () => {
         </SimpleGrid>
       </Container>
 
-      <Container mt="10" w="100%" bg={"#EDF2F7"}>
+      <Box className="container" mt="10" w="100%" bg={"#EDF2F7"}>
         <Box>
           <Heading>Technology Jobs</Heading>
         </Box>
         <Box className="cards">
-          <Stack spacing={3}>
-            <Box className="card1" w="100%" color={"black"} bg="white"></Box>
-          </Stack>
+          <Center>
+            <Wrap
+              w="460px"
+              border="2px"
+              borderColor="#CFBCF4"
+              p="2"
+              borderRadius="5"
+            >
+              <WrapItem w="50px">
+                <Image
+                  w="32px"
+                  h="32px"
+                  className=""
+                  src="https://www.wework4days.com/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fsrc%2Fassets%2Fimages%2Flogo.4cc40a9ad62028ac0ab9af556de0b081.svg&w=32&q=75"
+                  alt="Dan Abramov"
+                />
+              </WrapItem>
+              <WrapItem w="300px">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>Talewind</li>
+                  <li>Lead Producer</li>
+                  <li>£50k - £65k</li>
+                </ul>
+              </WrapItem>
+              <WrapItem w="70px">
+                <Box>
+                  <AiOutlineFire />
+                </Box>
+                <Box>
+                  <BiWorld />
+                </Box>
+                <Box>
+                  <AiFillPushpin />
+                </Box>
+                <Box>
+                  <FcBookmark />
+                </Box>
+              </WrapItem>
+            </Wrap>
+            {/* */}
+          </Center>
+          <Center>
+            <Wrap
+              w="460px"
+              border="2px"
+              borderColor="#CFBCF4"
+              p="2"
+              borderRadius="5"
+            >
+              <WrapItem w="50px">
+                <Image
+                  w="32px"
+                  h="32px"
+                  className=""
+                  src="https://www.wework4days.com/_next/image?url=https%3A%2F%2Fapi.wework4days.com%2Fassets%2Fd74e52d1-6529-4ab1-98b2-a27b1b9947ed&w=1920&q=75"
+                  alt="Dan Abramov"
+                />
+              </WrapItem>
+              <WrapItem w="300px">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>Talewind</li>
+                  <li>Lead Producer</li>
+                  <li>£50k - £65k</li>
+                </ul>
+              </WrapItem>
+              <WrapItem w="70px">
+                <Box>
+                  <AiOutlineFire />
+                </Box>
+                <Box>
+                  <BiWorld />
+                </Box>
+                <Box>
+                  <AiFillPushpin />
+                </Box>
+                <Box>
+                  <FcBookmark />
+                </Box>
+              </WrapItem>
+            </Wrap>
+            {/* */}
+          </Center>
+          <Center>
+            <Wrap
+              w="460px"
+              border="2px"
+              borderColor="#CFBCF4"
+              p="2"
+              borderRadius="5"
+            >
+              <WrapItem w="50px">
+                <Image
+                  w="32px"
+                  h="32px"
+                  className=""
+                  src="https://www.wework4days.com/_next/image?url=https%3A%2F%2Fapi.wework4days.com%2Fassets%2F0ecc5576-0a1c-4787-880b-6e23c379550e&w=1920&q=75"
+                  alt="Dan Abramov"
+                />
+              </WrapItem>
+              <WrapItem w="300px">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>Talewind</li>
+                  <li>Lead Producer</li>
+                  <li>£50k - £65k</li>
+                </ul>
+              </WrapItem>
+              <WrapItem w="70px">
+                <Box>
+                  <AiOutlineFire />
+                </Box>
+                <Box>
+                  <BiWorld />
+                </Box>
+                <Box>
+                  <AiFillPushpin />
+                </Box>
+                <Box>
+                  <FcBookmark />
+                </Box>
+              </WrapItem>
+            </Wrap>
+            {/* */}
+          </Center>
+          <Center>
+            <Wrap
+              w="460px"
+              border="2px"
+              borderColor="#CFBCF4"
+              p="2"
+              borderRadius="5"
+            >
+              <WrapItem w="50px">
+                <Image
+                  w="32px"
+                  h="32px"
+                  className=""
+                  src="https://www.wework4days.com/_next/image?url=https%3A%2F%2Fapi.wework4days.com%2Fassets%2F08360c55-4fb4-48e0-8392-eeda80628196&w=1920&q=75"
+                  alt="Dan Abramov"
+                />
+              </WrapItem>
+              <WrapItem w="300px">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>Talewind</li>
+                  <li>Lead Producer</li>
+                  <li>£50k - £65k</li>
+                </ul>
+              </WrapItem>
+              <WrapItem w="70px">
+                <Box>
+                  <AiOutlineFire />
+                </Box>
+                <Box>
+                  <BiWorld />
+                </Box>
+                <Box>
+                  <AiFillPushpin />
+                </Box>
+                <Box>
+                  <FcBookmark />
+                </Box>
+              </WrapItem>
+            </Wrap>
+            {/* */}
+          </Center>
+          <Center>
+            <Wrap
+              w="460px"
+              border="2px"
+              borderColor="#CFBCF4"
+              p="2"
+              borderRadius="5"
+            >
+              <WrapItem w="50px">
+                <Image
+                  w="32px"
+                  h="32px"
+                  className=""
+                  src="https://www.wework4days.com/_next/image?url=https%3A%2F%2Fapi.wework4days.com%2Fassets%2F5588c68a-7beb-4004-96ac-11b2ba547e5c&w=1920&q=75"
+                  alt="Dan Abramov"
+                />
+              </WrapItem>
+              <WrapItem w="300px">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>Talewind</li>
+                  <li>Lead Producer</li>
+                  <li>£50k - £65k</li>
+                </ul>
+              </WrapItem>
+              <WrapItem w="70px">
+                <Box>
+                  <AiOutlineFire />
+                </Box>
+                <Box>
+                  <BiWorld />
+                </Box>
+                <Box>
+                  <AiFillPushpin />
+                </Box>
+                <Box>
+                  <FcBookmark />
+                </Box>
+              </WrapItem>
+            </Wrap>
+            {/* */}
+          </Center>
+          <Center>
+            <Wrap
+              w="460px"
+              border="2px"
+              borderColor="#CFBCF4"
+              p="2"
+              borderRadius="5"
+            >
+              <WrapItem w="50px">
+                <Image
+                  w="32px"
+                  h="32px"
+                  className=""
+                  src="https://www.wework4days.com/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fsrc%2Fassets%2Fimages%2Flogo.4cc40a9ad62028ac0ab9af556de0b081.svg&w=32&q=75"
+                  alt="Dan Abramov"
+                />
+              </WrapItem>
+              <WrapItem w="300px">
+                <ul style={{ listStyleType: "none" }}>
+                  <li>Talewind</li>
+                  <li>Lead Producer</li>
+                  <li>£50k - £65k</li>
+                </ul>
+              </WrapItem>
+              <WrapItem w="70px">
+                <Box>
+                  <AiOutlineFire />
+                </Box>
+                <Box>
+                  <BiWorld />
+                </Box>
+                <Box>
+                  <AiFillPushpin />
+                </Box>
+                <Box>
+                  <FcBookmark />
+                </Box>
+              </WrapItem>
+            </Wrap>
+            {/* */}
+          </Center>
         </Box>
-      </Container>
+      </Box>
+      <Center>
+        <wrap>
+          <WrapItem justify="spaceAround">
+            <Box>Contact Us</Box>
+            <Box>Legal</Box>
+          </WrapItem>
+        </wrap>
+      </Center>
     </div>
   );
 };
+
+// <Stack spacing={3}>
+//   <Box className="card1" w="100%" color={"black"} bg="white">
+//     hello
+//   </Box>
+//   <Box className="card1" w="100%" color={"black"} bg="white">
+//     hello
+//   </Box>
+// </Stack>;
 
 // <Box className="">
 //         <Flex justify="space-between">
